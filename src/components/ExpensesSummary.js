962e2865a2008b1e9b2217b7ea01b58e2b7feda2
milos <obrovac.milos@gmail.com>
2020-01-28 13:05:22 +0100

    beautiful ExpensesSummary jsx

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import numeral from "numeral";
 import selectExpenses from "../selectors/expenses";
 import expensesTotal from "../selectors/expenses-total";
 
-export const ExpensesSummary = props => (
-  <div className="expenses-summary">
-    {props.listLength === 0 ? (
-      <p>The list is empty.</p>
-    ) : (
+export const ExpensesSummary = ({ totalAmount, expensesCount }) => {
+  const expenseWord = expensesCount === 1 ? "expense" : "expenses";
+  const formatedTotalAmount = numeral(totalAmount).format("$0,0.00");
+
+  return (
+    <div className="expenses-summary">
       <p>
-        Viewing {props.listLength} expense{props.listLength > 1 && "s "}{" "}
-        totalling ${props.amount.toFixed(2)}
+        {expensesCount === 0
+          ? `The list is empty.`
+          : `Viewing ${expensesCount} ${expenseWord} totalling ${formatedTotalAmount}`}
       </p>
-    )}
-  </div>
-);
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   const filteredExpenses = selectExpenses(state.expenses, state.filters);
   return {
-    amount: expensesTotal(filteredExpenses),
-    listLength: filteredExpenses.length
+    totalAmount: expensesTotal(filteredExpenses),
+    expensesCount: filteredExpenses.length
   };
 };
 
